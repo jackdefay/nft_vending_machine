3b2220723d977a5f6623185e901ca454f67b8d66
jackdefay <3jackattack3@gmail.com>
2024-04-06 14:08:26 -0400

    restructured and figured out how to call api functions

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,20 @@
 
 import Link from 'next/link'
 import Image from "next/image";
-import { showConnect } from './index.js';
+// import { showConnect } from './index.js';
+// import { openContractCall } from './index.js';
+// import { userSession } from '../userSession';
+// import {Button1} from './button.tsx';
+
+import { showConnect } from '@stacks/connect';
+import { userSession } from '../userSession';
+
+import { openContractCall } from '@stacks/connect';
+import { StacksTestnet } from '@stacks/network';
+import { AnchorMode, PostConditionMode, stringUtf8CV } from '@stacks/transactions';
+
+
+
 
 export default function Home() {
   return (
@@ -19,59 +32,60 @@ export default function Home() {
 
         <div className="topnav-right">
           <ul>
-            <li><a href="/login">login</a></li>
-            <li><a onClick={showConnect}>Connect Wallet</a></li>
+            {/* <li><a href="/login">login</a></li> */}
+            <li><button onClick={Button1}>Connect Wallet</button></li>
           </ul>
         </div>
       </div> 
 
-      <h1 className="text-5xl font-bold title">NFT Vending Machine</h1>
+      <h1 className="text-5xl font-bold title text-center">NFT Vending Machine</h1>
 
-      <Image src="/logo.png" alt="logo" width={72} height={16} />
-      
+      <h2 className="text-2xl font-bold title text-center">
+        <button onClick={Button2}>Dispense NFT</button>
+      </h2>
 
     </main>
   );
 }
 
-// function MyButton() {
 
-//   function handleClick() {
-//     showConnect();
-//   }
+export function Button1(){
+  return ( showConnect({
+    userSession, // `userSession` from previous step, to access storage
+    appDetails: {
+      name: 'My Stacks Web-App',
+      icon: "/logo.png", // shown in wallet pop-up
+    },
+    onFinish: () => {
+      window.location.reload(); // WHEN user confirms pop-up
+    },
+    onCancel: () => {
+      console.log('oops'); // WHEN user cancels/closes pop-up
+    },}))
+}
 
-//   return (
-//     <button onClick={handleClick}>Connect Wallet
-//     </button>
-//   );
-// }
+export function Button2(){
+  const pick = stringUtf8CV('Alice');
 
-// import { showConnect } from '@stacks/connect';
-// import { userSession } from './userSession';
+  return (
+    openContractCall({
+    network: new StacksTestnet(),
+    anchorMode: AnchorMode.Any, // which type of block the tx should be mined in
 
-// const myAppName = 'My Stacks Web-App'; // shown in wallet pop-up
-// const myAppIcon = window.location.origin + '/vercel.svg'; // shown in wallet pop-up
+    contractAddress: 'ST39MJ145BR6S8C315AG2BD61SJ16E208P1FDK3AK',
+    contractName: 'example-contract',
+    functionName: 'vote',
+    functionArgs: [pick],
 
-// showConnect({
-//   userSession, // `userSession` from previous step, to access storage
-//   appDetails: {
-//     name: myAppName,
-//     icon: myAppIcon,
-//   },
-//   onFinish: () => {
-//     window.location.reload(); // WHEN user confirms pop-up
-//   },
-//   onCancel: () => {
-//     console.log('oops'); // WHEN user cancels/closes pop-up
-//   },
-// });
+    postConditionMode: PostConditionMode.Deny, // whether the tx should fail when unexpected assets are transferred
+    postConditions: [], // for an example using post-conditions, see next example
 
-
-// or
-// const userAddresses = await window.LeatherProvider?.request('getAddresses');
-
-//or
-// const usersNativeSegwitAddress = userAddresses.results.addresses
-//   .find(address => address.type === 'p2wpkh');
-
-// const addressDetails = await fetch('https://mempool.space/api/address/' + usersNativeSegwitAddress)
+    onFinish: response => {
+      // WHEN user confirms pop-up
+    },
+    onCancel: () => {
+      // WHEN user cancels/closes pop-up
+    },
+  })
+  )
+}
