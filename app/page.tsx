8c9d977bacada741207d8545b3cb6004caea4a66
jackdefay <3jackattack3@gmail.com>
2024-04-11 00:23:52 -0400

    got a smart contract call working, when press button it executes a test function call and registers on the block explorer

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,17 @@ import { openContractCall } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 import { AnchorMode, PostConditionMode, stringUtf8CV } from '@stacks/transactions';
 
-import fetch from 'cross-fetch';
-import { Configuration, AccountsApi } from '@stacks/blockchain-api-client';
-
-
+import {
+  makeContractCall,
+  broadcastTransaction,
+  NonFungibleConditionCode,
+  createAssetInfo,
+  makeContractNonFungiblePostCondition,
+  FungibleConditionCode,
+  makeStandardSTXPostCondition,
+  bufferCVFromString,
+  bufferCV,
+} from '@stacks/transactions';
 
 
 export default function Home() {
@@ -75,10 +82,10 @@ export function Button2(){
     network: new StacksTestnet(),
     anchorMode: AnchorMode.Any, // which type of block the tx should be mined in
 
-    contractAddress: 'ST39MJ145BR6S8C315AG2BD61SJ16E208P1FDK3AK',
-    contractName: 'example-contract',
-    functionName: 'vote',
-    functionArgs: [pick],
+    contractAddress: 'ST13NS454WKV1X03E7VDQ990CFXFGCE5FZNRVE669',
+    contractName: 'generous-rose-turkey',
+    functionName: 'get-value',
+    functionArgs: [bufferCVFromString('test')], // arguments to the contract function
 
     postConditionMode: PostConditionMode.Deny, // whether the tx should fail when unexpected assets are transferred
     postConditions: [], // for an example using post-conditions, see next example
@@ -93,22 +100,93 @@ export function Button2(){
   )
 }
 
+// async function Button3(){
+//   // for mainnet, use `StacksMainnet()`
+//   const network = new StacksTestnet();
 
-(async () => {
-  const apiConfig = new Configuration({
-    fetchApi: fetch,
-    // for mainnet, replace `testnet` with `mainnet`
-    basePath: 'https://api.testnet.hiro.so', // defaults to http://localhost:3999
-  });
+//   // With a standard principal
+//   const postConditionAddress = 'SP2ZD731ANQZT6J4K3F5N8A40ZXWXC1XFXHVVQFKE';
+//   const postConditionCode = NonFungibleConditionCode.DoesNotSend;
+//   const assetAddress = 'SP62M8MEFH32WGSB7XSF9WJZD7TQB48VQB5ANWSJ';
+//   const assetContractName = 'test-asset-contract';
+//   const assetName = 'test-asset';
+//   const assetId = bufferCVFromString('test-token-asset-id');
+//   const nonFungibleAssetInfo = createAssetInfo(assetAddress, assetContractName, assetName);
 
-  // initiate the /accounts API with the basepath and fetch library
-  const accountsApi = new AccountsApi(apiConfig);
+//   // const standardNonFungiblePostCondition = makeStandardNonFungiblePostCondition(
+//   //   postConditionAddress,
+//   //   postConditionCode,
+//   //   nonFungibleAssetInfo,
+//   //   assetId
+//   // );
 
-  // get transactions for a specific account
-  const txs = await accountsApi.getAccountTransactions({
-    principal: 'ST13NS454WKV1X03E7VDQ990CFXFGCE5FZNRVE669',
-  });
-  // generous-rose-turkey
+//   // With a contract principal
+//   const contractAddress = 'SPBMRFRPPGCDE3F384WCJPK8PQJGZ8K9QKK7F59X';
+//   const contractName = 'test-contract';
 
-  console.log(txs);
-})().catch(console.error);
+//   const postConditions = makeContractNonFungiblePostCondition(
+//     contractAddress,
+//     contractName,
+//     postConditionCode,
+//     nonFungibleAssetInfo,
+//     assetId
+//   );
+
+//   const txOptions = {
+//     contractAddress: 'SPBMRFRPPGCDE3F384WCJPK8PQJGZ8K9QKK7F59X',
+//     contractName: 'contract_name',
+//     functionName: 'contract_function',
+//     functionArgs: [bufferCVFromString('foo')],
+//     senderKey: 'b244296d5907de9864c0b0d51f98a13c52890be0404e83f273144cd5b9960eed01',
+//     validateWithAbi: true,
+//     network,
+//     postConditions,
+//     anchorMode: AnchorMode.Any,
+//   };
+
+//   const transaction = await makeContractCall(txOptions);
+
+//   const broadcastResponse = await broadcastTransaction(transaction, network);
+//   const txId = broadcastResponse.txid;
+// }
+
+
+async function Button3() {
+  // for mainnet, use `StacksMainnet()`
+  const network = new StacksTestnet();
+
+  const arg = new Uint8Array([32]);
+  
+  // Add an optional post condition
+  // See below for details on constructing post conditions
+  const postConditionAddress = 'SP2ZD731ANQZT6J4K3F5N8A40ZXWXC1XFXHVVQFKE';
+  const postConditionCode = FungibleConditionCode.GreaterEqual;
+  const postConditionAmount = 1000000;
+  const postConditions = [
+    makeStandardSTXPostCondition(postConditionAddress, postConditionCode, postConditionAmount),
+  ];
+  
+  const txOptions = {
+    contractAddress: 'ST13NS454WKV1X03E7VDQ990CFXFGCE5FZNRVE669',
+    contractName: 'generous-rose-turkey',
+    functionName: 'get-value',
+    functionArgs: [bufferCV(arg)], //[bufferCVFromString('foo')],
+    senderKey: 'b244296d5907de9864c0b0d51f98a13c52890be0404e83f273144cd5b9960eed01',
+    validateWithAbi: true,
+    network,
+    postConditions,
+    anchorMode: AnchorMode.Any,
+  };
+  
+  const transaction = await makeContractCall(txOptions);
+
+  console.log('A');
+  
+  const broadcastResponse = await broadcastTransaction(transaction, network);
+  const txId = broadcastResponse.txid;
+
+  console.log(txId);
+  console.log(broadcastResponse);
+
+  return broadcastResponse;
+}
